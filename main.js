f16008e89863240352ccdff7b0114d54f48f33b6
Umar Hussain <umarhussain771@gmail.com>
2022-10-26 13:19:01 +0100

    Adds DOM manipulation to js file

---
+++ main.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form');
+const cardNumber = document.querySelector('.card-number');
+const numInputForm = document.getElementById('card_number');
+const cardName = document.querySelector('.name');
+const nameInputForm = document.getElementById('card_name');
+const cardExpMonth = document.querySelector('.month');
+const expMonthForm = document.getElementById('card_month');
+const cardExpYear = document.querySelector('.year');
+const expYearForm = document.getElementById('card_year');
+const cardCvc = document.querySelector('.cvc');
+const cvcInputForm = document.getElementById('cvc');
+const submitBtn = document.querySelector('submit-btn');
+const completed = document.querySelector('.thank-you')
+
